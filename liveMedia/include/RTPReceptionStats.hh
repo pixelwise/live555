9a68143115721bf9fd8a1582f786a9cd72f608b6
Daniel Oberhoff <daniel@danieloberhoff.de>
2020-02-27 10:06:45 +0100

    factor out RTPReceptionstats and simplifiy Boolean.hh

---
+++ liveMedia/include/RTPReceptionStats.hh
@@ -0,0 +1,84 @@
+
+#pragma once
+
+#include <cstdint>
+#include <sys/time.h>
+
+class RTPReceptionStats
+{
+public:
+  uint32_t SSRC() const { return fSSRC; }
+  unsigned numPacketsReceivedSinceLastReset() const {
+    return fNumPacketsReceivedSinceLastReset;
+  }
+  unsigned totNumPacketsReceived() const { return fTotNumPacketsReceived; }
+  double totNumKBytesReceived() const;
+
+  unsigned totNumPacketsExpected() const {
+    return (fHighestExtSeqNumReceived - fBaseExtSeqNumReceived) + 1;
+  }
+
+  unsigned baseExtSeqNumReceived() const { return fBaseExtSeqNumReceived; }
+  unsigned lastResetExtSeqNumReceived() const {
+    return fLastResetExtSeqNumReceived;
+  }
+  unsigned highestExtSeqNumReceived() const {
+    return fHighestExtSeqNumReceived;
+  }
+
+  unsigned jitter() const;
+
+  unsigned lastReceivedSR_NTPmsw() const { return fLastReceivedSR_NTPmsw; }
+  unsigned lastReceivedSR_NTPlsw() const { return fLastReceivedSR_NTPlsw; }
+  struct timeval const& lastReceivedSR_time() const {
+    return fLastReceivedSR_time;
+  }
+
+  unsigned minInterPacketGapUS() const { return fMinInterPacketGapUS; }
+  unsigned maxInterPacketGapUS() const { return fMaxInterPacketGapUS; }
+  struct timeval const& totalInterPacketGaps() const {
+    return fTotalInterPacketGaps;
+  }
+
+  RTPReceptionStats(uint32_t SSRC, uint16_t initialSeqNum);
+  RTPReceptionStats(uint32_t SSRC);
+  virtual ~RTPReceptionStats();
+  void noteIncomingPacket(
+    uint16_t seqNum, uint32_t rtpTimestamp,
+    unsigned timestampFrequency,
+    bool useForJitterCalculation,
+    struct timeval& resultPresentationTime,
+    bool& resultHasBeenSyncedUsingRTCP,
+    unsigned packetSize /* payload only */
+  );
+  void noteIncomingSR(uint32_t ntpTimestampMSW, uint32_t ntpTimestampLSW,
+          uint32_t rtpTimestamp);
+  void init(uint32_t SSRC);
+  void initSeqNum(u_int16_t initialSeqNum);
+  //// resets periodic stats (called after each reception report)
+  void reset();
+
+private:
+  uint32_t fSSRC;
+  unsigned fNumPacketsReceivedSinceLastReset;
+  unsigned fTotNumPacketsReceived;
+  uint32_t fTotBytesReceived_hi, fTotBytesReceived_lo;
+  bool fHaveSeenInitialSequenceNumber;
+  unsigned fBaseExtSeqNumReceived;
+  unsigned fLastResetExtSeqNumReceived;
+  unsigned fHighestExtSeqNumReceived;
+  int fLastTransit; // used in the jitter calculation
+  uint32_t fPreviousPacketRTPTimestamp;
+  double fJitter;
+  // The following are recorded whenever we receive a RTCP SR for this SSRC:
+  unsigned fLastReceivedSR_NTPmsw; // NTP timestamp (from SR), most-signif
+  unsigned fLastReceivedSR_NTPlsw; // NTP timestamp (from SR), least-signif
+  struct timeval fLastReceivedSR_time;
+  struct timeval fLastPacketReceptionTime;
+  unsigned fMinInterPacketGapUS, fMaxInterPacketGapUS;
+  struct timeval fTotalInterPacketGaps;
+  // Used to convert from RTP timestamp to 'wall clock' time:
+  bool fHasBeenSynchronized;
+  uint32_t fSyncTimestamp;
+  struct timeval fSyncTime;
+};
--- liveMedia/include/RTPSource.hh
+++ liveMedia/include/RTPSource.hh
@@ -21,19 +21,16 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _RTP_SOURCE_HH
 #define _RTP_SOURCE_HH
 
-#ifndef _FRAMED_SOURCE_HH
+#include "RTPReceptionStats.hh"
 #include "FramedSource.hh"
-#endif
-#ifndef _RTP_INTERFACE_HH
 #include "RTPInterface.hh"
-#endif
 
 class RTPReceptionStatsDB; // forward
 
 class RTPSource: public FramedSource {
 public:
   static Boolean lookupByName(UsageEnvironment& env, char const* sourceName,
-			      RTPSource*& resultSource);
+            RTPSource*& resultSource);
 
   Boolean curPacketMarkerBit() const { return fCurPacketMarkerBit; }
 
@@ -74,7 +71,7 @@ public:
   void setAuxilliaryReadHandler(AuxHandlerFunc* handlerFunc,
                                 void* handlerClientData) {
     fRTPInterface.setAuxilliaryReadHandler(handlerFunc,
-					   handlerClientData);
+             handlerClientData);
   }
 
   // Note that RTP receivers will usually not need to call either of the following two functions, because
@@ -85,7 +82,7 @@ public:
 
 protected:
   RTPSource(UsageEnvironment& env, Groupsock* RTPgs,
-	    unsigned char rtpPayloadFormat, u_int32_t rtpTimestampFrequency);
+      unsigned char rtpPayloadFormat, u_int32_t rtpTimestampFrequency);
       // abstract base class
   virtual ~RTPSource();
 
@@ -140,17 +137,17 @@ public:
 
   // The following is called whenever a RTP packet is received:
   void noteIncomingPacket(u_int32_t SSRC, u_int16_t seqNum,
-			  u_int32_t rtpTimestamp,
-			  unsigned timestampFrequency,
-			  Boolean useForJitterCalculation,
-			  struct timeval& resultPresentationTime,
-			  Boolean& resultHasBeenSyncedUsingRTCP,
-			  unsigned packetSize /* payload only */);
+        u_int32_t rtpTimestamp,
+        unsigned timestampFrequency,
+        Boolean useForJitterCalculation,
+        struct timeval& resultPresentationTime,
+        Boolean& resultHasBeenSyncedUsingRTCP,
+        unsigned packetSize /* payload only */);
 
   // The following is called whenever a RTCP SR packet is received:
   void noteIncomingSR(u_int32_t SSRC,
-		      u_int32_t ntpTimestampMSW, u_int32_t ntpTimestampLSW,
-		      u_int32_t rtpTimestamp);
+          u_int32_t ntpTimestampMSW, u_int32_t ntpTimestampLSW,
+          u_int32_t rtpTimestamp);
 
   // The following is called when a RTCP BYE packet is received:
   void removeRecord(u_int32_t SSRC);
@@ -172,90 +169,6 @@ protected:
 private:
   HashTable* fTable;
   unsigned fTotNumPacketsReceived; // for all SSRCs
-};
-
-class RTPReceptionStats {
-public:
-  u_int32_t SSRC() const { return fSSRC; }
-  unsigned numPacketsReceivedSinceLastReset() const {
-    return fNumPacketsReceivedSinceLastReset;
-  }
-  unsigned totNumPacketsReceived() const { return fTotNumPacketsReceived; }
-  double totNumKBytesReceived() const;
-
-  unsigned totNumPacketsExpected() const {
-    return (fHighestExtSeqNumReceived - fBaseExtSeqNumReceived) + 1;
-  }
-
-  unsigned baseExtSeqNumReceived() const { return fBaseExtSeqNumReceived; }
-  unsigned lastResetExtSeqNumReceived() const {
-    return fLastResetExtSeqNumReceived;
-  }
-  unsigned highestExtSeqNumReceived() const {
-    return fHighestExtSeqNumReceived;
-  }
-
-  unsigned jitter() const;
-
-  unsigned lastReceivedSR_NTPmsw() const { return fLastReceivedSR_NTPmsw; }
-  unsigned lastReceivedSR_NTPlsw() const { return fLastReceivedSR_NTPlsw; }
-  struct timeval const& lastReceivedSR_time() const {
-    return fLastReceivedSR_time;
-  }
-
-  unsigned minInterPacketGapUS() const { return fMinInterPacketGapUS; }
-  unsigned maxInterPacketGapUS() const { return fMaxInterPacketGapUS; }
-  struct timeval const& totalInterPacketGaps() const {
-    return fTotalInterPacketGaps;
-  }
-
-protected:
-  // called only by RTPReceptionStatsDB:
-  friend class RTPReceptionStatsDB;
-  RTPReceptionStats(u_int32_t SSRC, u_int16_t initialSeqNum);
-  RTPReceptionStats(u_int32_t SSRC);
-  virtual ~RTPReceptionStats();
-
-private:
-  void noteIncomingPacket(u_int16_t seqNum, u_int32_t rtpTimestamp,
-			  unsigned timestampFrequency,
-			  Boolean useForJitterCalculation,
-			  struct timeval& resultPresentationTime,
-			  Boolean& resultHasBeenSyncedUsingRTCP,
-			  unsigned packetSize /* payload only */);
-  void noteIncomingSR(u_int32_t ntpTimestampMSW, u_int32_t ntpTimestampLSW,
-		      u_int32_t rtpTimestamp);
-  void init(u_int32_t SSRC);
-  void initSeqNum(u_int16_t initialSeqNum);
-  void reset();
-      // resets periodic stats (called each time they're used to
-      // generate a reception report)
-
-protected:
-  u_int32_t fSSRC;
-  unsigned fNumPacketsReceivedSinceLastReset;
-  unsigned fTotNumPacketsReceived;
-  u_int32_t fTotBytesReceived_hi, fTotBytesReceived_lo;
-  Boolean fHaveSeenInitialSequenceNumber;
-  unsigned fBaseExtSeqNumReceived;
-  unsigned fLastResetExtSeqNumReceived;
-  unsigned fHighestExtSeqNumReceived;
-  int fLastTransit; // used in the jitter calculation
-  u_int32_t fPreviousPacketRTPTimestamp;
-  double fJitter;
-  // The following are recorded whenever we receive a RTCP SR for this SSRC:
-  unsigned fLastReceivedSR_NTPmsw; // NTP timestamp (from SR), most-signif
-  unsigned fLastReceivedSR_NTPlsw; // NTP timestamp (from SR), least-signif
-  struct timeval fLastReceivedSR_time;
-  struct timeval fLastPacketReceptionTime;
-  unsigned fMinInterPacketGapUS, fMaxInterPacketGapUS;
-  struct timeval fTotalInterPacketGaps;
-
-private:
-  // Used to convert from RTP timestamp to 'wall clock' time:
-  Boolean fHasBeenSynchronized;
-  u_int32_t fSyncTimestamp;
-  struct timeval fSyncTime;
 };
 
 
